9453c6925d60b3d87b2b0dc8f653364ddaec227c
Joe Gregorio <joe@bitworking.org>
2016-09-11 04:52:33 +0000

    remove httplib2

--- projects/httplib2/test17.py
+++
@@ -1,5 +0,0 @@
-import httplib2
-httplib2.debuglevel = 5
-h = httplib2.Http()
-h.add_credentials("joe.gregorio", "jcg@q4q4")
-print h.request("https://www.google.com/history/lookup?month=1&day=3&yr=2007&output=rss")
