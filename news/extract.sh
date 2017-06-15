751752788a187b5a39a2d1c857abeac5665ab583
Joe Gregorio <joe@bitworking.org>
2017-06-15 16:38:59 -0400

    extract.sh now puts the news/ga stories in the right place.

--- news/extract.sh
+++ news/extract.sh
@@ -14,6 +14,11 @@ for i in $(find -name \*.html); do # Not recommended, will break on whitespace
   body=`grep -Pzo "(?s)(?<=\<body\>).+(?=\<\/body\>)" $i`
   printf "\n---\nlayout: post\ntitle: %s\n---\n" "$title"
   echo ../../newblog/_posts/$date-$filename.html
-  printf "%s\nlayout: post\ntitle: %s\n---\n" "---" "$title" > ../../newblog/_posts/$date-$filename.html
+  extra=""
+  if [[ $i == *"/ga/"* ]]; then
+    extra=`printf "permalink: /news/ga/$filename.html\n"`
+  fi
+
+  printf "%s\nlayout: post\ntitle: %s\n%s\n---\n" "---" "$title" "$extra" > ../../newblog/_posts/$date-$filename.html
   echo "$body" >>  ../../newblog/_posts/$date-$filename.html
 done
