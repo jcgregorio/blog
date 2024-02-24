d93b4b50df7c6f45e8f5c6550b8b9afd3c67ed9f
Joe Gregorio <joe@bitworking.org>
2024-02-24 18:29:46 -0500

    DFS works

--- content/news/2024/02/gantt/graph.ts
+++ content/news/2024/02/gantt/graph.ts
@@ -16,21 +16,74 @@ type Graph = {
   Edges: Edges;
 };
 
-function edgesToMap(edges: Edges): { [index: number]: Edge[] } {
-  const ret: { [index: number]: Edge[] } = {};
+function edgesSrcToMap(edges: Edges): Map<number, Edges> {
+  const ret = new Map<number, Edges>();
 
   edges.forEach((e: Edge) => {
-    const arr = ret[e.i] || [];
+    const arr = ret.get(e.i) || [];
     arr.push(e);
-    ret[e.i] = arr;
+    ret.set(e.i, arr);
   });
 
   return ret;
 }
 
+function edgesDstToMap(edges: Edges): Map<number, Edges> {
+  const ret = new Map<number, Edges>();
+
+  edges.forEach((e: Edge) => {
+    const arr = ret.get(e.j) || [];
+    arr.push(e);
+    ret.set(e.j, arr);
+  });
+
+  return ret;
+}
+
+type vertexFunction = (v: Vertex, index: number) => boolean;
+
+function DFS(g: Graph, start_vertex: number, f: vertexFunction) {
+  const edgesBySrc = edgesSrcToMap(g.Edges);
+
+  const visit = (index: number) => {
+    f(g.Vertices[index], index);
+    const next = edgesBySrc.get(index);
+    if (next === undefined) {
+      return;
+    }
+    next.forEach((e: Edge) => {
+      visit(e.j);
+    });
+  };
+
+  visit(start_vertex);
+}
+
+const G: Graph = {
+  Vertices: [{ weight: 0 }, { weight: 10 }, { weight: 20 }, { weight: 15 }],
+  Edges: [
+    { i: 0, j: 1 },
+    { i: 0, j: 2 },
+    { i: 1, j: 3 },
+    { i: 2, j: 3 },
+  ],
+};
+
+DFS(G, 0, (v: Vertex, index: number) => {
+  console.log(v.weight, index);
+  return true;
+});
+
+/*
 function IsDAG(g: Graph): boolean {
   const seen: Vertex[] = [];
-  const edgeMap = edgesToMap(g.Edges);
-  g.Vertices.forEach((v: Vertex) => {});
+  const edgeMap = edgesSrcToMap(g.Edges);
+  g.Vertices.forEach((v: Vertex, i: number) => {
+    const edges = edgeMap.get(i);
+    if (edges === undefined) {
+      return;
+    }
+  });
   return true;
 }
+*/
