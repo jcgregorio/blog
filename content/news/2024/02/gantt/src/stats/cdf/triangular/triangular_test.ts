28a9aec51ad4eee02efd524b6dbe6a9ce01aa69d
Joe Gregorio <joe@bitworking.org>
2024-05-25 10:47:00 -0400

    clean up testa

--- content/news/2024/02/gantt/src/stats/cdf/triangular/triangular_test.ts
+++ content/news/2024/02/gantt/src/stats/cdf/triangular/triangular_test.ts
@@ -2,7 +2,8 @@
 import { assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
 import { Triangular } from "./triangular.ts";
 
-Deno.test("url test", () => {
+Deno.test("Test with simple case c=b.", () => {
   const t = new Triangular(0, 1, 1);
   assertEquals(0, t.sample(0));
+  assertEquals(1, t.sample(1));
 });
