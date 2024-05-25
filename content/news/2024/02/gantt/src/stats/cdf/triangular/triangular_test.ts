8ca6487162ce011fc5e5f22791731ce35304e5c6
Joe Gregorio <joe@bitworking.org>
2024-05-25 11:00:33 -0400

    more tests

--- content/news/2024/02/gantt/src/stats/cdf/triangular/triangular_test.ts
+++ content/news/2024/02/gantt/src/stats/cdf/triangular/triangular_test.ts
@@ -1,9 +1,11 @@
 // url_test.ts
 import { assertEquals } from "https://deno.land/std@0.224.0/assert/mod.ts";
 import { Triangular } from "./triangular.ts";
+import { assertAlmostEquals } from "https://deno.land/std@0.224.0/assert/assert_almost_equals.ts";
 
 Deno.test("Test with simple case c=b.", () => {
   const t = new Triangular(0, 1, 1);
   assertEquals(0, t.sample(0));
+  assertAlmostEquals(Math.SQRT2 / 2, t.sample(0.5));
   assertEquals(1, t.sample(1));
 });
