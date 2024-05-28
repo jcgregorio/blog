c5dd26e3c7943b5058eeff15a7dd452174540b80
Joe Gregorio <joe@bitworking.org>
2024-05-28 14:02:50 -0400

    breaking out tests.

--- content/news/2024/02/gantt/src/chart/chart_test.ts
+++ content/news/2024/02/gantt/src/chart/chart_test.ts
@@ -1,6 +1,35 @@
 import { assert } from "https://deno.land/std@0.224.0/assert/assert.ts";
 import { Validate } from "./chart.ts";
 import { Chart } from "./chart.ts";
+import { DirectedGraph } from "../dag/dag.ts";
+import { assertFalse } from "https://deno.land/std@0.224.0/assert/assert_false.ts";
+
+Deno.test("A directed graph validates:", () => {
+  const G: DirectedGraph = {
+    Vertices: [{}, {}, {}, {}],
+    Edges: [
+      { i: 0, j: 1 },
+      { i: 0, j: 2 },
+      { i: 1, j: 3 },
+      { i: 2, j: 3 },
+    ],
+  };
+  assert(Validate(G));
+});
+
+Deno.test("A directed graph with a loop fails to validate:", () => {
+  const GWithLoop: DirectedGraph = {
+    Vertices: [{}, {}, {}, {}],
+    Edges: [
+      { i: 0, j: 1 },
+      { i: 0, j: 2 },
+      { i: 1, j: 3 },
+      { i: 2, j: 3 },
+      { i: 2, j: 0 },
+    ],
+  };
+  assertFalse(Validate(GWithLoop));
+});
 
 Deno.test("A default chart validates.", () => {
   const r = Validate(new Chart());
